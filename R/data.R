2a8092fbd125e3129069e7d22864212a8bef1a21
ZibeiLin <linzibei610@gmail.com>
2019-11-19 11:49:09 +1100

    first commit

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Days of the week
+#'
+#' The seven days of the week, in abbreviated form.
+#'
+#' @format A vector of length 7#'
+#'
+#' @source Own knowledge
+"days"
---
+++ R/zbl_functions.R
@@ -0,0 +1,13 @@
+#' Title
+#' To return a sqaured root value
+#'
+#' @param x input a value
+#'
+#' @return
+#' @export
+#'
+#' @examples zbl_sqr(4)
+#'
+zbl_sqr <- function(x) {
+  sqrt(x)
+}
